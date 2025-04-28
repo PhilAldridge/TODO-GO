116a96b381b0bc583dd70e5c51426908a3b19fed
Phil.Aldridge <phil_mj12@yahoo.co.uk>
2025-04-28 17:17:51 +0100

    added V2 concurrency test

--- main_test.go
+++ main_test.go
@@ -3,12 +3,15 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"strconv"
 	"testing"
 
+	"github.com/PhilAldridge/TODO-GO/auth"
 	"github.com/PhilAldridge/TODO-GO/lib"
 	"github.com/PhilAldridge/TODO-GO/router"
 	"github.com/PhilAldridge/TODO-GO/store"
@@ -26,8 +29,8 @@ func TestConcurrentPutAndGet(t *testing.T) {
 	server.Handle("/Todos/", &api)
 	server.Handle("/Todos", &api)
 
-	for i := 0; i < 10; i++ {
-		t.Run("ParallelTest", func(t *testing.T) {
+	for i := 0; i < 50; i++ {
+		t.Run("ParallelTestV1", func(t *testing.T) {
 			t.Parallel()
 
 			payload := router.V1PutBody{
@@ -52,6 +55,72 @@ func TestConcurrentPutAndGet(t *testing.T) {
 			if w.Code != http.StatusOK {
 				t.Errorf("GET: unexpected status: %d", w.Code)
 			}
+		})
+	}
+}
+
+func TestConcurrentPutAndGetMultipleUsers(t *testing.T) {
+	server := http.NewServeMux()
+	todoStore, userStore := store.NewSQLStore()
+	todoApi := router.NewV2ApiHandler(todoStore)
+	server.Handle("/TodosV2/", auth.JWTMiddleware(&todoApi))
+	server.Handle("/TodosV2", auth.JWTMiddleware(&todoApi))
+
+	userApi := router.NewUserApiHandler(userStore)
+	server.Handle("/Users/", &userApi)
+	server.Handle("/Users", &userApi)
+
+	for i := 2; i < 50; i++ {
+		t.Run("ParallelTestV2", func(t *testing.T) {
+			t.Parallel()
+
+			payload := router.UserPutBody{
+				Username: "user" + strconv.Itoa(i),
+				Password: "password" + strconv.Itoa(i),
+			}
+			b, _ := json.Marshal(payload)
+
+			//Create user
+			req:= httptest.NewRequest(http.MethodPut, "/Users", bytes.NewBuffer(b))
+			req.Header.Set("Content-Type", "application/json")
+			w:= httptest.NewRecorder()
+			server.ServeHTTP(w, req)
+
+			//Login
+			req= httptest.NewRequest(http.MethodPost, "/Users", bytes.NewBuffer(b))
+			req.Header.Set("Content-Type", "application/json")
+			w= httptest.NewRecorder()
+			server.ServeHTTP(w, req)
+
+			if w.Code != http.StatusOK {
+				t.Errorf("Login: unexpected status: %d", w.Code)
+			}
+
+			resBody, err := io.ReadAll(w.Body)
+			if err != nil {
+				t.Errorf("client: could not read response body: %s\n", err)
+
+			}
+
+			//Use JWT to authorise PUT method in V2API
+			jwt:= string(resBody)
+			fmt.Println(jwt)
+			V2payload:= router.V1PutBody{
+				Label:    "test" + strconv.Itoa(i),
+				Deadline: "2025-01-01",
+			}
+			b,_= json.Marshal(V2payload)
+
+			req= httptest.NewRequest(http.MethodPut, "/TodosV2", bytes.NewBuffer(b))
+			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set("Authorization","Bearer "+jwt)
+			w = httptest.NewRecorder()
+			server.ServeHTTP(w, req)
+
+			if w.Code != http.StatusOK {
+				t.Errorf("Put: unexpected status: %d", w.Code)
+			}
+
 		})
 	}
 }
